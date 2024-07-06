d1480914664964ec2fcdfd4ed340b79d1fd10d9d
TEJAS <tbomble6292@gmail.com>
2024-07-06 14:28:32 +0530

    Functionality to redirect to redirect to login page according to user logged in status using Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shop_a_z/auth/auth_service.dart';
 import 'package:shop_a_z/pages/dashboard_page.dart';
 import 'package:shop_a_z/pages/login_page.dart';
 import 'firebase_options.dart';
@@ -28,14 +29,22 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  final _router = GoRouter(routes: [
-    GoRoute(
-        name: DashBoardPage.routeName,
-        path: DashBoardPage.routeName,
-        builder: (context, state) => const DashBoardPage()),
-    GoRoute(
-        name: LoginPage.routeName,
-        path: LoginPage.routeName,
-        builder: (context, state) => const LoginPage())
-  ]);
+  final _router = GoRouter(
+      initialLocation: DashBoardPage.routeName,
+      redirect: (context, state) {
+        if (AuthService.currentUser == null) {
+          return LoginPage.routeName;
+        }
+        return null;
+      },
+      routes: [
+        GoRoute(
+            name: DashBoardPage.routeName,
+            path: DashBoardPage.routeName,
+            builder: (context, state) => const DashBoardPage()),
+        GoRoute(
+            name: LoginPage.routeName,
+            path: LoginPage.routeName,
+            builder: (context, state) => const LoginPage())
+      ]);
 }
