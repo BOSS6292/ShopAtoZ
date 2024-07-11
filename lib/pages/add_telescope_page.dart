3f7454c2e7342fd8970d77df940def4b063d7a0f
TEJAS <tbomble6292@gmail.com>
2024-07-11 13:24:11 +0530

    Declared Constants and Properties

--- lib/pages/add_telescope_page.dart
+++ lib/pages/add_telescope_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_a_z/models/brand.dart';
+import 'package:shop_a_z/utils/constants.dart';
 
 class AddTeleScope extends StatefulWidget {
   static const String routeName = 'addtelescope';
@@ -9,11 +11,26 @@ class AddTeleScope extends StatefulWidget {
 }
 
 class _AddTeleScopeState extends State<AddTeleScope> {
+  final _modelController = TextEditingController();
+  final _dimensionController = TextEditingController();
+  final _weightController = TextEditingController();
+  final _lensDiameterController = TextEditingController();
+  final _priceController = TextEditingController();
+  final _stockController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  Brand? brand;
+  String? imageLocalPath;
+  DateTime? dateTime;
+  String mountDescription = TelescopeUtils.mountList.first;
+  String focusType = TelescopeUtils.mountList.first;
+  String telescopeType = TelescopeUtils.mountList.first;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AddTeleScopes'),
+        title: const Text('Add TeleScopes'),
       ),
       body: const Center(
         child: Text('Add TeleScopes'),
